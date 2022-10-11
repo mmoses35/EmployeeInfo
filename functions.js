db9e9194e311156d20a13e4279a3989dedb0bdc5
Malcolm I Moses <mmoses77104@viterbo.edu>
2022-10-11 12:15:31 -0500

    first commit

---
+++ functions.js
@@ -0,0 +1,20 @@
+function doInputOutput() {
+  let width= document.getElementById("width").value;
+   var total= houseVolume(width, depth, height, sweep);
+   
+}
+function houseVolume(width, depth, height, sweep) {
+    
+    let roofVolume= triangleArea * width;
+    
+}
+function livingVolune(width, depth, height) {
+    let livingVolume= width * depth * height;
+
+}
+function roofVolume(width, depth, sweep) {
+    
+}
+function triangleArea (a, b, c) {
+    
+}
