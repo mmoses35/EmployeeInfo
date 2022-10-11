8c9867e39d3a7faca9a26ae6de0d88f834ce4831
Malcolm I Moses <mmoses77104@viterbo.edu>
2022-10-11 16:23:18 -0500

    finished

--- functions.js
+++ functions.js
@@ -1,20 +1,35 @@
-function doInputOutput() {
-  let width= document.getElementById("width").value;
-   var total= houseVolume(width, depth, height, sweep);
-   
+var height
+var depth
+var width
+var sweep
+
+
+function doInputOutput() {;
+  let depth=parseInt(document.getElementById("depth").value);
+  let width=parseInt(document.getElementById("width").value);
+  let height=parseInt(document.getElementById("height").value);
+  let sweep=parseInt(document.getElementById("sweep").value);
+  let total= houseVolume(width, depth, height, sweep);
+   alert (total);
 }
 function houseVolume(width, depth, height, sweep) {
-    
-    let roofVolume= triangleArea * width;
-    
+    let LV= livingVolume(width, depth, height);
+    let RV= triangleArea(width, depth, height) * width;
+    let HV= LV + RV;
+    return (HV);
 }
-function livingVolune(width, depth, height) {
-    let livingVolume= width * depth * height;
-
+function livingVolume(width, depth, height) {
+    let LV= width * depth * height;
+    return (LV);    
 }
 function roofVolume(width, depth, sweep) {
-    
+    let TA= triangleArea(sweep, sweep, depth);  
+    let RV= TA * width;
+    return(RV)
 }
 function triangleArea (a, b, c) {
-    
+
+   let s= (a + b + c)/2;
+   let TA= Math.sqrt(s*(s-a)*(s-b)*(s-c));
+   return (TA);
 }
